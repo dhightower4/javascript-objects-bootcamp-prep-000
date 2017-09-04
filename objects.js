3dbd7058e31c3aa1419b5cbcd8321e5586e35eed
Donte Hightower <dontehightower12@gmail.com>
2017-09-04 00:51:05 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,8 +4,11 @@ function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
   return playlist
 }
+<<<<<<< HEAD
 
 function removeFromPlaylist(playlist, artistName) {
     delete playlist[artistName]
     return playlist
 }
+=======
+>>>>>>> d7c34ccfd2798fbcf0489686761396ad3780cae2
